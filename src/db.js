0fb2536dae4af651fb8c47af9c080b8a48edd08c
paulpacaud <85497912+paulpacaud@users.noreply.github.com>
2024-04-10 21:13:02 +0200

    added onds and airlines to 456qwerty

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ const API_KEYS = {
     "123azerty" : { airlines : ["NI", "AT", "I2"], onds : ['LYS-AGA'] },
     "456qwerty" : {
         airlines : [
-        "EN", "AY", "2C", "LG", "TK", "SV", "AH", "U2", "SN", "DY", "LY", "LX", "OU", "CL", "EE", "AT", "NI", "ET", "TU", "EI", "JU", "WY", "LA"
+        "EN", "AY", "2C", "LG", "TK", "SV", "AH", "U2", "SN", "DY", "LY", "LX", "OU", "CL", "EE", "AT", "NI", "ET", "TU", "EI", "JU", "WY", "LA", "BF", "SS", "AF", "AC", "SS", "TU", "UU", "AT", "AT", "UX", "SS", "TS", "TX", "TP", "TX", "AZ", "IB", "LH", "MK", "I2", "TP", "TP", "GF", "AF", "AF", "TK", "UA", "LH", "AF", "TK"
         ],
-        onds : ['NTE-JTR', 'LYS-LIS', 'PAR-TLV', 'PAR-PMI', 'LYS-DXB', 'BSL-JCN', 'LYS-DKR', 'NCE-MOW', 'BSL-PAR', 'PAR-DUS', 'PAR-SEZ', 'CFE-INN', 'PAR-AUH', 'PAR-RAK', 'BOD-VCE', 'PAR-YWG', 'PAR-TOS', 'PAR-KRK', 'BIA-LYS', 'TLS-SJO', 'PAR-NYC', 'PUF-PAR', 'PAR-DAR', 'TUF-LON', 'LYS-UIO', 'MRS-ALG', 'NTE-IST', 'PAR-VCE', 'PAR-TLS', 'PAR-YBG']
+        onds : ['NTE-JTR', 'LYS-LIS', 'PAR-TLV', 'PAR-PMI', 'LYS-DXB', 'BSL-JCN', 'LYS-DKR', 'NCE-MOW', 'BSL-PAR', 'PAR-DUS', 'PAR-SEZ', 'CFE-INN', 'PAR-AUH', 'PAR-RAK', 'BOD-VCE', 'PAR-YWG', 'PAR-TOS', 'PAR-KRK', 'BIA-LYS', 'TLS-SJO', 'PAR-NYC', 'PUF-PAR', 'PAR-DAR', 'TUF-LON', 'LYS-UIO', 'MRS-ALG', 'NTE-IST', 'PAR-VCE', 'PAR-TLS', 'PAR-YBG', "PAR-RUN", "PAR-YMQ", "PAR-PTP", "PAR-TUN", "PAR-RAK", "PAR-MAD", "PAR-FDF", "PAR-LIS", "PAR-ROM", "PAR-OPO", "PAR-DXB", "PAR-IST"]
     },
     "admin": { airlines : [], onds : [] }
 };
